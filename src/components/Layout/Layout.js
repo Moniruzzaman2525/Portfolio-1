fa7f806a639a7f94390c1d6c198c17d51ac51a36
unknown <md616264@gmail.com>
2022-06-05 12:09:14 +0600

    service part

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import About from '../About/About';
 import Header from '../Header/Header';
 import HeroSection from '../Herosection/HeroSection';
+import Services from '../Services/Services';
 
 const Layout = () => {
     return <Fragment>
         <Header></Header>
         <HeroSection></HeroSection>
         <About></About>
+        <Services />
     </Fragment>
 };
 
---
+++ src/components/Services/Services.js
@@ -0,0 +1,51 @@
+
+
+
+import React from 'react';
+import './Services.css'
+import { Container, Row, Col } from 'reactstrap';
+
+
+const serviceData = [
+    {
+        icon: 'ri-code-line',
+        title: 'Web Design',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et consequuntur accusantium sit quos tempora eveniet mollitia dolore!'
+    },
+
+    {
+        icon: 'ri-code-s-slash-line',
+        title: 'Web Development',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et consequuntur accusantium sit quos tempora eveniet mollitia dolore!'
+    }
+]
+
+const Services = () => {
+    return <section id='services'>
+        <Container>
+            <Row>
+                <Col lg='12' className='services_top mb-5'>
+                    <h6>Features</h6>
+                    <h2>What services I Pprovide</h2>
+                </Col>
+                {
+                    serviceData.map((item, index) => (
+                        <Col lg='6' md='6' sm='6' key={index} className='mb-4'>
+                            <div className="single_service">
+                                <span className="service_icon">
+                                    <i class={item.icon}></i>
+                                </span>
+                                <h2>{item.title}</h2>
+                                <p>{item.desc}</p>
+
+                            </div>
+                        </Col>
+                    ))
+                }
+
+            </Row>
+        </Container>
+    </section>
+};
+
+export default Services;
